03b5d7c92c83ec0e7bc1fccd0d9c50425cfff1a0
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-06 20:15:25 +0530

    Done templete.pug and index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const port = 80;
+
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    const title = "Home";
+    res.status(200).render("index.pug", title);
+});
+
+app.post("/register", (req, res) => {
+
+});
+
+app.listen(port, () => {
+    console.log(`Server has been started at port ${port}`);
+})
